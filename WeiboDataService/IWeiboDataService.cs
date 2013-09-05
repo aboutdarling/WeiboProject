7816d1a02cddc59ef11ed9459da27df0a980d50b
Qing Li <qing.li@ef.com>
2013-09-05 18:08:36 +0800

    add one interface

---
+++ WeiboDataService/IWeiboDataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModel;
+
+namespace Weibo.DataAccess
+{
+    interface IWeiboDataService
+    {
+        IList<WeiboData> GetData();
+
+    }
+}
