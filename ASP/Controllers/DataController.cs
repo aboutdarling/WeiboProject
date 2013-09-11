9c1493a2c4ee02819f524b54b223c4dc133da0e5
Qing Li <qing.li@ef.com>
2013-09-11 16:14:42 +0800

    add asp project

---
+++ ASP/Controllers/DataController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataModel;
+using ReadSQL;
+using Weibo.DataAccess;
+namespace ASP.Controllers
+{
+    public class DataController : ApiController
+    {
+        private static IWeiboDataService dataService = new WeiboDataService();
+        private IEnumerable<WeiboData> weiboDataList = dataService.GetData();
+
+        /* /api/data */
+        public IEnumerable<WeiboData> GetAllWeiboData()
+        {
+            return weiboDataList;
+        }
+
+        /* /api/data/id */
+        public WeiboData GetWeiboById(int id)
+        {
+            var weiboData = new WeiboData();
+            foreach (var currentdata in weiboDataList)
+            {
+                if (currentdata.weiboID == id)
+                {
+                    weiboData = currentdata;
+                    break;
+                }
+            }
+            if (weiboData == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return weiboData;
+        }
+    }
+}
